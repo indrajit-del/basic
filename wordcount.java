4b728e16ce8a8a6ed4d309122db907d7005ecdfa
indrajit-del <72353250+indrajit-del@users.noreply.github.com>
2023-02-24 13:38:29 +0530

    Add files via upload

---
+++ wordcount.java
@@ -0,0 +1,32 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class incrasing {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s="information technology";
+        String p="seacom engineering collage";
+        System.out .println("above". compareTo("able"));
+        //System.out .println(s.eqals(p));
+    }
+}
+import java.util.*;
+public class specialNo{
+    public static void main(String[] args) 
+    {
+      //  Scanner sc=new sc(system.in);
+        String a =" this is my book ";
+        a = a.trim();
+       int l=a.length();int count=1,i;
+       for( i=0;i<l;i++)
+       {
+           if((a.charAt(i) == ' ')&&(a.charAt(i+1) != ' '))
+           {
+             count++;  
+           }
+        
+        }System.out.println("the word number is :"+count);
+    }
+        
+    }
